09315ee1b990db8e4d492e9a17e7619275ba70c8
con0fav <connor.favero@gmail.com>
2020-11-11 19:23:23 -0800

    removed test, added script.js started pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+// all functionality on the front page index
+// navbar, with page information and about button
+// WHEN about is pressed: modal/text box pops up containing names of group members
+
+// front page functionality:
+// 
