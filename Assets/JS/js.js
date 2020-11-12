0e6db2d9215efded19de771baed9b6b1f042494c
Zac Edens <themancalledzac@gmail.com>
2020-11-11 21:34:03 -0800

    Triposo API addition.

--- Assets/JS/js.js
+++ Assets/JS/js.js
@@ -3,3 +3,50 @@
 
 
 // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Zac's playground
+
+// https://www.triposo.com/api/
+// account ID: 7ZTLRO4H
+// yx4cnfzccsyrugqslwb2eqc2s92obaye
+
+// https://www.triposo.com/api/20200803/location.json?id=London&account=<account ID>&token=<API token>
+var chosenLocation = "Amsterdam";
+var triposoAPIKey = "yx4cnfzccsyrugqslwb2eqc2s92obaye";
+var triposoAPIToken = "7ZTLRO4H";
+var triposoURL = "https://www.triposo.com/api/20200803/location.json?id=" + chosenLocation + "&account=" + triposoAPIKey + "&token=" + triposoAPIToken;
+
+console.log(triposoURL);
