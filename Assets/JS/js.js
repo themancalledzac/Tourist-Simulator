6203eca9eb8260bbf7ba7494824533bf11c985eb
goeringkenneth <goeringkenneth@gmail.com>
2020-11-14 10:48:13 -0800

    weather api

--- Assets/JS/js.js
+++ Assets/JS/js.js
@@ -76,3 +76,113 @@ var triposoURL = "https://www.triposo.com/api/20200803/location.json?id=" + chos
 
 console.log(triposoURL);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var APIKey = "a0ed00a1e03e86452a0e4c5419b896b8";
+
+//location input
+
+var location=input
+
+// openweathermap.org
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +location; "&appid=" + APIKey;
+
+
+    $.ajax({
+
+      url: queryURL,
+
+      method: "GET"
+
+    }).then(function(response) {
+
+      console.log(queryURL)
+      console.log(response)
+
+
+
+     var tempF=(response.main.temp-273.15)* 1.80 + 32;
+
+
+    $(".#").html("<h1>"+response.name +" "+ "Weather Details</h1>");
+
+    $(".#").text("Temperature:"+" "+tempF.toFixed(0));
+
+    $(".#").text("Humitidty:"+" "+response.main.humidity);
+
+    $(".#").text("Wind speed:"+" "+response.wind.speed.toFixed(0));
+
+    });
+
+
+
