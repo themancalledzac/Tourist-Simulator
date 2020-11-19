a48664d5507195c3d783b3deebb684868e8b7e96
Zac Edens <themancalledzac@gmail.com>
2020-11-18 19:06:45 -0800

    click button works again after the reorg.

--- Assets/JS/js.js
+++ Assets/JS/js.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
 var cities = [];
-var citySearchButton = $("citySearchBtn");
-var citySearchBox = $("citySearch");
+var citySearchButton = $("#citySearchBtn");
+var citySearchBox = $("#citySearch");
 
 // GLOBAL triposo API variables
 var triposoAPIKey = "7ZTLRO4H";
@@ -14,7 +14,7 @@ var WeatherAPIKey = "a0ed00a1e03e86452a0e4c5419b896b8";
 
 // TriposoAPI Ajax call
 function triposoAPI( city ) {
-    $("#searchedCity").empty();
+    // $("#searchedCity").empty();
   for (var i = 0; i < cities.length; i++) {
 
     var city = cities[i];
@@ -26,17 +26,17 @@ function triposoAPI( city ) {
            
           
           // city name found at: response.results[0].id
-          var cityEl = response.results[0].id;
-          // country name found at: response.results[0].country_id
-          var countryEl = response.results[0].country_id;
-          // cityImage  
-          var imgURL = response.results[0].images[0].source_url;
+          // var cityEl = response.results[0].id;
+          // // country name found at: response.results[0].country_id
+          // var countryEl = response.results[0].country_id;
+          // // cityImage  
+          // var imgURL = response.results[0].images[0].source_url;
 
-          var city = { 'name': cityEl, 'country': countryEl, 'image': imgURL }
-          response.results[0].id;
-          // localStorage
-          localStorage.setItem("cityName", cityName);
-
+          // var city = { 'name': cityEl, 'country': countryEl, 'image': imgURL }
+          // response.results[0].id;
+          // // localStorage
+          // localStorage.setItem('city', city);
+          console.log(localStorage)
         });
     }
 
@@ -47,7 +47,7 @@ function triposoAPI( city ) {
 
 
 // WeatherAPIKey API AJAX CALL
-function weatherAPI ( city ) {
+function weatherAPI ( response ) {
     var city = cities[i];
     var WeatherAPIKey = "a0ed00a1e03e86452a0e4c5419b896b8";
     var weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WeatherAPIKey;
@@ -57,10 +57,10 @@ function weatherAPI ( city ) {
       $("#citySearchBtn").on("click", function(event) {
           event.preventDefault();
           //      var tempF=(response.main.temp-273.15)* 1.80 + 32;          
-          //     $(".#").html("<h1>"+response.name +" "+ "Weather Details</h1>");          
-          //     $(".#").text("Temperature:"+" "+tempF.toFixed(0));          
-          //     $(".#").text("Humitidty:"+" "+response.main.humidity);       
-          //     $(".#").text("Wind speed:"+" "+response.wind.speed.toFixed(0));
+              // $(".#").html("<h1>"+response.name +" "+ "Weather Details</h1>");          
+              // $(".#").text("Temperature:"+" "+tempF.toFixed(0));          
+              // $(".#").text("Humitidty:"+" "+response.main.humidity);       
+              // $(".#").text("Wind speed:"+" "+response.wind.speed.toFixed(0));
 
 
           // MORE than likely, we will need to stringify localStorage data so we can add this data to that data
@@ -90,7 +90,9 @@ function displayCity () {
 
 
 function previousCity () {
-
+  // clear local storage
+  // push cities array to localStorage
+  
 
 };
 
@@ -100,22 +102,24 @@ function previousCity () {
 // Click event listerner to our search button.
 citySearchButton.on("click", function(event) {
     event.preventDefault();
-    
   // This line grabs the input from the textbox
   var city = citySearchBox.val().trim();
   var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
 
   // Adding movie from the textbox to our array
   cities.push(capitalizeCity);
+  localStorage.setItem("city", capitalizeCity);
   
   // Calling renderButtons which handles the processing of our movie array
-  triposoAPI(capitalizeCity);
+  triposoAPI( capitalizeCity );
+  // weatherAPI();
+        //---------------------ONCE FINISHED---------------------------//
+  displayCity();
+  previousCity();
   // console.log(city);
+  console.log(city)
 });
 
-
-// Adding movie from the textbox to our array
-cities.push(capitalizeCity);
 
 
 
