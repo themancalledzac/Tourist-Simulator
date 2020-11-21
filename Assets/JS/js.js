d1a3dc4f66bd74b374bd3bc24a44eeb4f904c867
con0fav <connor.favero@gmail.com>
2020-11-21 01:20:25 -0800

    testing previous city function and click event w/ placeholder sidebar

--- Assets/JS/js.js
+++ Assets/JS/js.js
@@ -39,7 +39,7 @@ function geoAPI( capitalizeCity ) {
 
     $("#textMain").addClass("box");
 
-    console.log(response.data);
+    // console.log(response.data);
   });
 
 };
@@ -55,18 +55,18 @@ function weatherAPI (city) {
   // console.log(cityEl);
   var WeatherAPIKey = "a0ed00a1e03e86452a0e4c5419b896b8";
   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + WeatherAPIKey;
-  console.log(weatherURL);
+  // console.log(weatherURL);
    
   $.ajax({ 
     url: weatherURL, 
     method: "GET" 
   }).then(function(response) {
-    console.log(response)
+    // console.log(response)
     var tempF = (response.main.temp);          
     $("#weatherData").text("Temperature: " + tempF);  
     var imageURL = response.weather[0].icon;
     var iconImg = $("<img>").attr("src","http://openweathermap.org/img/wn/"+imageURL+"@2x.png");
-          console.log(imageURL)
+          // console.log(imageURL)
     $("#weatherData").append(iconImg) 
     $("#citySearchBtn").on("click", function(event) {
       event.preventDefault();  
@@ -82,7 +82,7 @@ function weatherAPI (city) {
 // TriposoAPI Ajax call
 function triposoAPI( capitalizeCity ) {
   var triposoURL = "https://www.triposo.com/api/20200803/location.json?id=" + capitalizeCity + "&account=" + triposoAPIKey + "&token=" + triposoAPIToken;  
-  console.log(triposoURL);
+  // console.log(triposoURL);
   $.ajax({ url: triposoURL,  method: "GET" 
       }).then(function(response) {       
         var imgURL = response.results[0].images[0].source_url;
@@ -96,46 +96,69 @@ function triposoAPI( capitalizeCity ) {
         // localStorage
         // cities.push(cityObject);
         // localStorage.setItem('city', cityObject);
-        console.log(response.results[0].country_id);
+        // console.log(response.results[0].country_id);
       }); 
-};
-
-
-
-
-
-
-// Click event listerner to our search button.
-$("#citySearchBtn").click(function(event) {
-  event.preventDefault();
-// This line grabs the input from the textbox
-var city = $("input[name=citySearch]").val();
-console.log(city)
-var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
-  // This line grabs the input from the textbox
-  var city = citySearchBox.val().trim();
-  var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
-  var cityAllOne = capitalizeCity;
-  cityAllOne = cityAllOne.replace(/ /g, "_");
-
-  // Adding movie from the textbox to our array
-  cities.push(capitalizeCity);
-  localStorage.setItem('cities', cities);
-  // localStorage.setItem('city', capitalizeCity);
-  // Calling renderButtons which handles the processing of our movie array
-  geoAPI( city );
-  weatherAPI( city );
-  triposoAPI( capitalizeCity );
-  //---------------------ONCE FINISHED---------------------------//
-  // displayCity();
-  // previousCity();
-  // console.log(city);
-  console.log(city)
-});
-
-
-
-// ------------------------BEHOLD. MY FUTURE STUFF-------------------------------------------------------//
+    };
+    
+    
+    
+    
+    
+    // Click event listerner to our search button.
+    $("#citySearchBtn").click(function(event) {
+      event.preventDefault();
+      // This line grabs the input from the textbox
+      var city = $("input[name=citySearch]").val();
+      // console.log(city)
+      var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
+      // This line grabs the input from the textbox
+      var city = citySearchBox.val().trim();
+      var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
+      var cityAllOne = capitalizeCity;
+      cityAllOne = cityAllOne.replace(/ /g, "_");
+      
+      // Adding city from the textbox to our array
+      cities.push(capitalizeCity);
+      localStorage.setItem('cities', JSON.stringify(cities));
+      // localStorage.setItem('city', capitalizeCity);
+      // Calling rendering functions which handles the processing of our city array
+      geoAPI( city );
+      weatherAPI( city );
+      triposoAPI( capitalizeCity );
+      //---------------------ONCE FINISHED---------------------------//
+      // displayCity();
+      // previousCity();
+      // console.log(city);
+    });
+    
+    function previousCity () {
+      
+        // using name of city in local storage
+        var prevSearch = JSON.parse(localStorage.getItem('cities'));
+        // link is made
+        var searchLink = $("<button class='btn border mt-1 bg-white rounded' style='width: 12rem;'>").text(prevSearch);
+        var searchList = $("<li>");
+        searchList.append(searchLink);
+        $("#history").prepend(searchList)
+        // when link in previous city sidebar is clicked
+        
+        // send to api calls
+        // then name of city gets called into triposo and weather
+        triposoAPI(capitalizeCity);
+        
+        weatherAPI(city);
+        
+        // and displays a card with that city's information
+    };
+    
+    $("#history").on('click', '.btn', function(event) {
+      event.preventDefault();
+          console.log($(this).text());
+          geoAPI($(this).text());
+      
+      });
+    
+    // ------------------------BEHOLD. MY FUTURE STUFF-------------------------------------------------------//
 
 
 // Google image API
@@ -157,10 +180,3 @@ var capitalizeCity = city.charAt(0).toUpperCase() + city.slice(1);
 
 
 
-
-// function previousCity () {
-//   // clear local storage
-//   // push cities array to localStorage
-  
-
-// };
